96c5fe6aa36c943c1733591366f9862509c4d77e
OmarElsakka1 <s-omar.elsakka@zewailcity.edu.eg>
2024-06-13 16:36:54 -0700

    valid email

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,13 @@
+-- Creating a trigger on an UPDATE event
+-- Creating a Trigger on users table;
+
+DELIMITER $$ ;
+CREATE TRIGGER validate_email
+BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+	IF NEW.email != OLD.email THEN
+	   SET NEW.valid_email = 0;
+	END IF;
+END;
+$$
