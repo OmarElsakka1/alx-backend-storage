55914ceeafbfaea4b9bfa0180af4fc4aa654047e
OmarElsakka1 <s-omar.elsakka@zewailcity.edu.eg>
2024-06-13 17:04:37 -0700

    glam rock

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,2 +1,3 @@
 -- Script listing all bands with Glam rock as the main style, rankde by longevity
-SELECT band_name, IF(ISNULL(split),YEAR(NOW()),split)- formed AS lifespan FROM metal_bands WHERE style LIKE '%Glam rock%' ORDER BY lifespan DESC;
+SELECT band_name, (IFNULL(split, 2022) - formed)
+AS lifespan FROM metal_bands WHERE style LIKE '%Glam rock%' ORDER BY lifespan DESC;
